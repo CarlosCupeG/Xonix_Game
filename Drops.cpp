746668b2fb352fcaf2547408d0b26b4f36019720
CarlosCupeG <carlosdaniel922@live.com>
2018-12-01 08:13:43 -0500

    H

---
+++ Drops.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zero on 01/12/18.
+//
+
+#include "Drops.h"
---
+++ Drops.h
@@ -0,0 +1,14 @@
+//
+// Created by zero on 01/12/18.
+//
+
+#ifndef XONIX_DROPS_H
+#define XONIX_DROPS_H
+
+
+class Drops {
+
+};
+
+
+#endif //XONIX_DROPS_H
